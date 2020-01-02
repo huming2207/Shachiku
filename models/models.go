5e59274a50446b1c075c08f9bb0a9d380fa179b7
Jackson Ming Hu <huming2207@gmail.com>
2020-01-02 17:27:29 +0800

    models: implement config singleton

--- models/models.go
+++ models/models.go
@@ -6,39 +6,42 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"gopkg.in/ini.v1"
 	"log"
-	"os"
 	"shachiku/common"
 )
 
-
-
 var db *gorm.DB
+var cfgFile *ini.File
 
 func GetDb() *gorm.DB {
 	var err error
-	dialect := os.Getenv(common.DatabaseDialect)
+	cfg := GetConfig().Section(common.DatabaseSection)
+	dialect := cfg.Key(common.DatabaseDialect).String()
 
-	if db != nil{
+	if db != nil {
 		return db
 	} else if dialect == "sqlite3" {
-		db, err = gorm.Open("sqlite3", common.DatabasePath)
+		log.Println("Connecting to SQLite3 database")
+		db, err = gorm.Open("sqlite3", cfg.Key(common.DatabasePath).String())
 	} else if dialect == "postgres" {
+		log.Println("Connecting to PostgreSQL database")
 		db, err = gorm.Open("postgres",
-			fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s",
-				os.Getenv(common.DatabaseHost),
-				os.Getenv(common.DatabasePort),
-				os.Getenv(common.DatabaseUser),
-				os.Getenv(common.DatabaseName),
-				os.Getenv(common.DatabasePassword)))
+			fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=disable",
+				cfg.Key(common.DatabaseHost).String(),
+				cfg.Key(common.DatabasePort).String(),
+				cfg.Key(common.DatabaseUser).String(),
+				cfg.Key(common.DatabaseName).String(),
+				cfg.Key(common.DatabasePassword).String()))
 	} else if dialect == "mysql" {
+		log.Println("Connecting to MySQL database")
 		db, err = gorm.Open("mysql",
 			fmt.Sprintf("%s:%s@(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-				os.Getenv(common.DatabaseUser),
-				os.Getenv(common.DatabasePassword),
-				os.Getenv(common.DatabaseHost),
-				os.Getenv(common.DatabasePort),
-				os.Getenv(common.DatabaseName)))
+				cfg.Key(common.DatabaseUser).String(),
+				cfg.Key(common.DatabasePassword).String(),
+				cfg.Key(common.DatabaseHost).String(),
+				cfg.Key(common.DatabasePort).String(),
+				cfg.Key(common.DatabaseName).String()))
 	}
 
 	if err != nil {
@@ -46,4 +49,18 @@ func GetDb() *gorm.DB {
 	}
 
 	return db
+}
+
+func GetConfig() *ini.File {
+	if cfgFile != nil {
+		return cfgFile
+	} else {
+		var err error
+		cfgFile, err = ini.Load("config.ini")
+		if err != nil {
+			log.Fatalln("Failed to open configuration file: %w", err)
+		}
+	}
+
+	return cfgFile
 }
