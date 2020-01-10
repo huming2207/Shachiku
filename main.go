1656fd115e9990632c38fbca950f7215987eb817
Jackson Ming Hu <huming2207@gmail.com>
2020-01-10 09:36:30 +0800

    main: register handlers

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 	"log"
+	"shachiku/handlers/auth"
+	"shachiku/handlers/portal"
 )
 
 func main() {
@@ -12,6 +14,9 @@ func main() {
 	// Register middleware
 	router.Use(middleware.Logger())
 	router.Use(middleware.Recover())
+
+	auth.RegisterHandler(router)
+	portal.RegisterHandler(router)
 
 	err := router.Start("0.0.0.0:3000")
 	if err != nil {
