a8153f49b3b112f463c8ed85d25c2677314fc909
Jackson Ming Hu <huming2207@gmail.com>
2020-01-11 09:25:17 +0800

    task: add json tags

--- models/task.go
+++ models/task.go
@@ -7,13 +7,13 @@ import (
 
 type Task struct {
 	gorm.Model
-	Title     string `gorm:"column:title;not null"`
-	Location  string `gorm:"column:location"`
-	Owner     User   `gorm:"foreignkey:OwnerID"`
-	OwnerID   uint
-	Attendees []*User   `gorm:"many2many:users_tasks;"`
-	StartAt   time.Time `gorm:"column:start_at;not null"`
-	EndAt     time.Time `gorm:"column:end_at"`
-	Comment   string    `gorm:"column:comment"`
-	Tags      []*Tag    `gorm:"many2many:users_tags"`
+	Title     string    `gorm:"column:title;not null" json:"title"`
+	Location  string    `gorm:"column:location" json:"location"`
+	Owner     User      `gorm:"foreignkey:OwnerID" json:"owner"`
+	OwnerID   uint      `json:"-"`
+	Attendees []*User   `gorm:"many2many:users_tasks;" json:"attendees"`
+	StartAt   time.Time `gorm:"column:start_at;not null" json:"start_at"`
+	EndAt     time.Time `gorm:"column:end_at" json:"end_at"`
+	Comment   string    `gorm:"column:comment" json:"comment"`
+	Tags      []*Tag    `gorm:"many2many:users_tags" json:"tags"`
 }
