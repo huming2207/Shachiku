9c9fd632bce030f3b512430adc3e99aa68e252cd
Jackson Ming Hu <huming2207@gmail.com>
2020-01-13 10:30:59 +0800

    model: tag: unify JSON serialise naming

--- models/tag.go
+++ models/tag.go
@@ -2,6 +2,11 @@ package models
 
 type Tag struct {
 	Model
-	Name  string  `gorm:"column:name;not null"`
-	Tasks []*Task `gorm:"many2many:tags_tasks"`
+	Name  string  `gorm:"column:name;not null" json:"name"`
+	Tasks []*Task `gorm:"many2many:tags_tasks" json:"tasks"`
+}
+
+func (ctx *Tag) LoadTasks() error {
+	db := GetDb()
+	return db.Preload("Tasks").First(ctx).Error
 }
