16f7dd04e3312caa76a93c98dd9c221a2a08c14c
Jackson Ming Hu <huming2207@gmail.com>
2020-01-13 10:32:12 +0800

    handlers: tags: implement list all tags handler

--- handlers/portal/tag.go
+++ handlers/portal/tag.go
@@ -1,9 +1,33 @@
 package portal
 
-import "github.com/labstack/echo/v4"
+import (
+	"github.com/labstack/echo/v4"
+	"net/http"
+	"shachiku/common"
+	"shachiku/models"
+)
 
 func listTags(ctx echo.Context) error {
-	return nil
+	tags := &[]models.Tag{}
+	db := models.GetDb()
+
+	err := db.Find(&tags).Error
+	if err != nil {
+		return ctx.JSON(http.StatusInternalServerError, common.J{
+			"message": "Failed to load tasks",
+		})
+	}
+
+	for idx, _ := range *tags {
+		err = (*tags)[idx].LoadTasks()
+		if err != nil {
+			return ctx.JSON(http.StatusInternalServerError, common.J{
+				"message": "Failed to load related tasks",
+			})
+		}
+	}
+
+	return ctx.JSON(http.StatusOK, tags)
 }
 
 func addTag(ctx echo.Context) error {
